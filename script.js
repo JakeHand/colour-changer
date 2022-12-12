157eff3f830259781df9bef5d230651a39c74bdd
Jake Hand <jakehandp@gmail.com>
2022-12-12 17:20:52 +0800

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let red = document.getElementById("red");
+let green = document.getElementById("green");
+let blue = document.getElementById("blue");
+
+red.onclick = function() {
+    document.body.style.backgroundColor = "red";
+}
+
+green.onclick = function() {
+    document.body.style.backgroundColor = "green";
+}
+
+blue.onclick = function() {
+    document.body.style.backgroundColor = "blue";
+}
